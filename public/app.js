d0f500ba0bc1f178d1edad996300ae6bb318ea11
Barbara <barbara.branco.c@gmail.com>
2022-03-25 16:35:28 +0100

    scrollTriggerReplace

--- public/app.js
+++ public/app.js
@@ -1,7 +1,85 @@
+let slidePosition = 0;
+
+const slides = document.getElementsByClassName('carousel__item');
+
+const totalSlides = slides.length;
+
+// console.log(slides);
+
+document.getElementById('carousel-next').addEventListener("click", function () {
+
+  moveToNextSlide();
+  console.log("hello next");
+
+});
+
+
+console.log("hello previous")
+document.getElementById('carousel-prev').addEventListener("click", function () {
+
+  moveToPrevSlide();
+});
+
+
+
+
+//move the slides
+
+function updateSlidePosition() {
+
+  for (let slide of slides) {
+
+    slide.classList.remove('carousel__item--visible');
+
+    slide.classList.add('carousel__item--hidden');
+
+  };
+
+  slides[slidePosition].classList.add('carousel__item--visible');
+
+};
+
+function moveToNextSlide() {
+
+  if (slidePosition === totalSlides - 1) {
+
+    slidePosition = 0;
+
+  } else {
+
+    slidePosition++;
+
+  };
+
+  updateSlidePosition();
+
+};
+
+function moveToPrevSlide() {
+
+  if (slidePosition === 0) {
+
+    slidePosition = totalSlides - 1;
+
+  } else {
+
+    slidePosition--;
+
+  };
+
+  updateSlidePosition();
+
+};
+
+
+
+
+
+
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector(".closeIcon");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
 
 
@@ -24,25 +102,36 @@ hamburger.addEventListener("click", toggleMenu);
 
 
 
-menuItems.forEach( 
-    function(menuItem) { 
-      menuItem.addEventListener("click", toggleMenu);
-    }
+menuItems.forEach(
+  function (menuItem) {
+    menuItem.addEventListener("click", toggleMenu);
+  }
 )
 
 
+gsap.registerPlugin(ScrollTrigger)
 
-let controller = new ScrollMagic.Controller();
+gsap.to(".div1", {
+    y:-500,
+    duration:3,
+    scrollTrigger: ".poppop"
+})
+
+
+/* let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-.to(".poppop", 3 ,{y:-600})
-.fromTo(".grid",{y:0, duration:3} )
-.to(".test_glass", 3, {top:"0%"}, "-=3")
-.fromTo(".ccontainer-fluid",{opacity:0}, {opacity:1, duration:3})
+//.to(".text", 2,{x:500})//(grab the file, add duration,and what do we want to do:we want to change the osition of x to 500)
+//.fromTo(".text", {opacity:0}, {opacity:1, duration:3})//opacity starts at 0, ends at 1, for the duration of 3sec
+//.to(".content-images", 2,{opacity:0},"-=2");//last param makes it start at the same time as first one,-2 sec, so same as .text
+.to(".horseRider", 3, {y:-300})//.horseRider moves up 300
+.to(".horseDrinking", 3, {y:-200}, "-=3")//.horseDrinking moves up 200
+.fromTo(".bg1",{y:-50},{y:0, duration:3},"-=3" )//moves background from y -50 to y 0, the same duration as rest (3)
+.to(".content", 3, {top:"0%"}, "-=3")//after setting Pin to element section, we want the .content(images of mountains) to move all the way to the top (0%) of section at the same time as animation , so -=3
+.fromTo(".content-images",{opacity:0}, {opacity:1, duration:3})
 .fromTo(".text",{opacity:0}, {opacity:1, duration:3})
 
-//scrollMagic
 let scene = new ScrollMagic.Scene({//we create a new scene, an animation
   triggerElement:"section",//when do we want it to start? meaning here immediatly in this case
   duration:"300%",//at what speed you want the animations to be activated
@@ -53,7 +142,7 @@ let scene = new ScrollMagic.Scene({//we create a new scene, an animation
 .setTween(timeline)//hook up the animation to scene
 .addTo(controller)
 //now we want to pin page
-.setPin("section")
+.setPin("section") */
 
 //code copied from plantchi
 /* const hoverImage = document.querySelector('.accordionContainer');
@@ -67,83 +156,10 @@ const hoverImageOpen = document.querySelector('__1');
 
 //carrousel Nisrine
 
-let slidePosition = 0;
-
-const slides = document.getElementsByClassName('carousel__item');
-
-const totalSlides = slides.length;
-
-// console.log(slides);
-
-document.getElementById('carousel-next').addEventListener("click", function(){
-
-moveToNextSlide();
-
-});
-
-
-document.getElementById('carousel-prev').addEventListener("click", function(){
-
-moveToPrevSlide();
-
-});
-
-//move the slides
-
-function updateSlidePosition(){
-
-for (let slide of slides){
-
-slide.classList.remove('carousel__item--visible');
-
-slide.classList.add('carousel__item--hidden');
-
-};
-
-slides[slidePosition].classList.add('carousel__item--visible');
-
-};
-
-function moveToNextSlide(){
-
-if(slidePosition === totalSlides -1){
-
-slidePosition = 0;
-
-} else{
-
-slidePosition++;
-
-};
-
-updateSlidePosition();
-
-};
-
-function moveToPrevSlide(){
-
-if(slidePosition === 0 ){
-
-slidePosition = totalSlides -1;
-
-} else{
-
-slidePosition--;
-
-};
-
-updateSlidePosition();
-
-};
 
 
 
-//using materiallibrary
-/* const mdcFoo = require('@material/foo');
-const MDCFoo = mdcFoo.MDCFoo;
-const MDCFooFoundation = mdcFoo.MDCFooFoundation;
 
-const foo = new MDCFoo(document.querySelector('.mdc-foo')); */
 
 
 
